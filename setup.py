9177eeace8456a0ff821f8efc7c9a080fe471083
Nico Hübschmann <nichue@liquitcore.com>
2020-05-31 17:45:33 +0200

    [RELEASE] Add additional information for release v1.0.0

--- setup.py
+++ setup.py
@@ -8,14 +8,16 @@ from setuptools import setup, find_packages
 
 setup(name='drf-rest-auth',
       version='1.0.0',
+      license='MIT',
       author='Nico Huebschmann',
       author_email='nico_huebschmann@web.de',
       python_requires='>=3.6.0',
       url='https://github.com/LiQuitCore/drf-rest-auth',
+      download_url = 'https://github.com/LiQuitCore/drf-rest-auth/archive/v1.0.0.tar.gz',
       description='Offers a set of REST API endpoints for authentication',
       platforms='any',
       packages=find_packages(exclude=['tests']),
-      keywords='django rest auth rest-framework jwt api',
+      keywords=['django', 'rest-framework', 'REST', 'auth', 'JWT', 'authentication'],
       install_requires=[
           'Django>=3.0.4',
           'djangorestframework>=3.11.0',
@@ -26,6 +28,8 @@ setup(name='drf-rest-auth',
           'Intended Audience :: Developers',
           'Intended Audience :: System Administrators',
           'Operating System :: OS Independent',
-          'Topic :: Software Development'
+          'Topic :: Software Development',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
       ],
       )
